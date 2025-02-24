2c9ac67771a915df782bfdee542faf5ee7f6bb4c
noxyid66 <167220482+noxyid66@users.noreply.github.com>
2025-02-24 15:44:08 +0700

    claim_faucet_thp.js
    run javascript in console

---
+++ claim_faucet_thp.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+document.getElementById("req").click();
+console.log('%cmessage:', 'color: crimson; font-size: 10px; font-style: bold;');
+console.log("request thp succes send faucet to metamask check!!!");
+console.log(new Date().toString());
+}, 65000);
